55deb89e7c0b849d2aa82796c7c41a3e7df8095f
Anton Gushcha <ncrashed@protonmail.com>
2026-06-08 16:18:43 +0300

    chore(nix+cabal): Add flake to pinned Nix toolchain and remove -O2 by default

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,3 +13,6 @@ Initial release of `bytestring-mmap-compat`, forked from
     `default-language: Haskell2010`, explicit base / bytestring
     bounds.
   - Public API unchanged from upstream 0.2.2.
+  - Dropped `-O2` from `ghc-options` (a per-package `-O2` is rarely
+    a real win on a thin FFI wrapper and burns compile time on
+    every downstream user; the hot path is in `cbits/`).
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "bytestring-mmap-compat";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hpkgs = pkgs.haskellPackages;
+        pkg = hpkgs.callCabal2nix "bytestring-mmap-compat" ./. { };
+      in {
+        packages.default = pkg;
+
+        devShells.default = hpkgs.shellFor {
+          packages = _: [ pkg ];
+          buildInputs = [
+            hpkgs.cabal-install
+            hpkgs.ghc
+            hpkgs.haskell-language-server
+            pkgs.pkg-config
+          ];
+        };
+      });
+}
